26a4cac88982f097a7509737dc7795ab4638756d
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-07-31 12:45:08 -0400

    basic mint house scaffolding

---
+++ packages/contracts/src/MintHouse.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/// @title A Mint House
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+// LICENSE
+// MintHouse.sol is a modified version of Zora's AuctionHouse.sol:
+// https://github.com/ourzora/auction-house/blob/54a12ec1a6cf562e49f0a4917990474b11350a2d/contracts/AuctionHouse.sol
+//
+// MintHouse.sol source code Copyright Zora licensed under the GPL-3.0 license.
+
+pragma solidity ^0.8.22;
+
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+import { IMintHouse } from "./interfaces/IMintHouse.sol";
+import { ICultureIndex } from "./interfaces/ICultureIndex.sol";
+
+import { UUPS } from "./proxy/UUPS.sol";
+
+//TODO use 1e6 for bps - to mimic splits
+
+contract MintHouse is IMintHouse, UUPS, PausableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
+    // The minimum price accepted in a mint
+    uint256 public mintPrice;
+
+    // The split of the winning bid that is reserved for the creator of the Art Piece in basis points
+    uint256 public creatorRateBps;
+
+    // The all time minimum split of the winning bid that is reserved for the creator of the Art Piece in basis points
+    uint256 public minCreatorRateBps;
+
+    // The duration of a single mint
+    uint256 public duration;
+
+    // The time between each mint
+    uint256 public mintInterval;
+
+    // The active mint
+    IMintHouse.Mint public mint;
+
+    ///                                                          ///
+    ///                         CONSTRUCTOR                      ///
+    ///                                                          ///
+
+    constructor() payable initializer {}
+
+    ///                                                          ///
+    ///                         INITIALIZER                      ///
+    ///                                                          ///
+
+    /**
+     * @notice Initialize the mint house and base contracts,
+     * populate configuration values, and pause the contract.
+     * @dev This function can only be called once.
+     * @param _initialOwner The address of the owner.
+     * @param _mintParams The mint params for mints.
+     */
+    function initialize(address _initialOwner, MintParams calldata _mintParams) external initializer {
+        __Pausable_init();
+        __ReentrancyGuard_init();
+        __Ownable_init(_initialOwner);
+
+        _pause();
+
+        if (_mintParams.creatorRateBps < _mintParams.minCreatorRateBps) revert CREATOR_RATE_TOO_LOW();
+
+        // set mint params
+        mintPrice = _mintParams.mintPrice;
+        duration = _mintParams.duration;
+
+        // set creator payout params
+        creatorRateBps = _mintParams.creatorRateBps;
+        minCreatorRateBps = _mintParams.minCreatorRateBps;
+    }
+
+    /**
+     * @notice Create a new mint.
+     */
+    function createNewMint() external override nonReentrant whenNotPaused {
+        // TODO add checks here
+
+        _createMint();
+    }
+
+    /**
+     * @notice Pause the Mint house.
+     * @dev This function can only be called by the owner when the
+     * contract is unpaused. While no new mints can be started when paused,
+     * anyone can settle an ongoing mint.
+     */
+    function pause() external override onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @notice Set the split of the winning bid that is reserved for the creator of the Art Piece (token) in basis points.
+     * @dev Only callable by the owner.
+     * @param _creatorRateBps New creator rate in basis points.
+     */
+    function setCreatorRateBps(uint256 _creatorRateBps) external onlyOwner {
+        if (_creatorRateBps < minCreatorRateBps) revert CREATOR_RATE_TOO_LOW();
+
+        if (_creatorRateBps > 10_000) revert INVALID_BPS();
+        creatorRateBps = _creatorRateBps;
+
+        emit CreatorRateBpsUpdated(_creatorRateBps);
+    }
+
+    /**
+     * @notice Set the minimum split of the winning bid that is reserved for the creator of the Art Piece (token) in basis points.
+     * @dev Only callable by the owner.
+     * @param _minCreatorRateBps New minimum creator rate in basis points.
+     */
+    function setMinCreatorRateBps(uint256 _minCreatorRateBps) external onlyOwner {
+        if (_minCreatorRateBps > creatorRateBps) revert MIN_CREATOR_RATE_ABOVE_CREATOR_RATE();
+
+        if (_minCreatorRateBps > 10_000) revert INVALID_BPS();
+
+        //ensure new min rate cannot be lower than previous min rate
+        if (_minCreatorRateBps <= minCreatorRateBps) revert MIN_CREATOR_RATE_NOT_INCREASED();
+
+        minCreatorRateBps = _minCreatorRateBps;
+
+        emit MinCreatorRateBpsUpdated(_minCreatorRateBps);
+    }
+
+    /**
+     * @notice Unpause the Mint house.
+     * @dev This function can only be called by the owner when the
+     * contract is paused. If required, this function will start a new mint.
+     */
+    function unpause() external override onlyOwner {
+        _unpause();
+
+        if (mint.startTime == 0 || mint.settled) {
+            _createMint();
+        }
+    }
+
+    /**
+     * @notice Set the mint mint price.
+     * @dev Only callable by the owner.
+     */
+    function setMintPrice(uint256 _mintPrice) external override onlyOwner {
+        mintPrice = _mintPrice;
+
+        emit MintPriceUpdated(_mintPrice);
+    }
+
+    /**
+     * @notice Create a mint.
+     * @dev Store the mint details in the `mint` state variable and emit an MintCreated event.
+     * If the mint reverts, the minter was updated without pausing this contract first. To remedy this,
+     * catch the revert and pause this contract.
+     */
+    function _createMint() internal {
+        // TODO fill in
+        // try revolutionToken.mint() returns (uint256 tokenId) {
+        //     uint256 startTime = block.timestamp;
+        //     uint256 endTime = startTime + duration;
+        //     mint = Mint({
+        //         tokenId: tokenId,
+        //         amount: 0,
+        //         startTime: startTime,
+        //         endTime: endTime,
+        //         bidder: payable(0),
+        //         settled: false,
+        //         referral: payable(0)
+        //     });
+        //     emit MintCreated(tokenId, startTime, endTime);
+        // } catch {
+        //     _pause();
+        // }
+    }
+
+    ///                                                          ///
+    ///                        DROP UPGRADE                   ///
+    ///                                                          ///
+
+    /// @notice Ensures the caller is authorized to upgrade the contract
+    /// @dev This function is called in `upgradeTo` & `upgradeToAndCall`
+    /// @param _newImpl The new implementation address
+    function _authorizeUpgrade(address _newImpl) internal view override onlyOwner {}
+}
---
+++ packages/contracts/src/interfaces/IMintHouse.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/// @title Interface for Gnars Mint Houses
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+pragma solidity ^0.8.22;
+
+interface IMintHouseEvents {
+    event MintCreated(uint256 indexed tokenId, uint256 startTime, uint256 endTime);
+
+    event MintSettled(
+        uint256 indexed tokenId,
+        address winner,
+        uint256 amount,
+        uint256 pointsPaidToCreators,
+        uint256 ethPaidToCreators
+    );
+
+    event MintPriceUpdated(uint256 mintPrice);
+
+    event CreatorRateBpsUpdated(uint256 rateBps);
+
+    event MinCreatorRateBpsUpdated(uint256 rateBps);
+}
+
+interface IMintHouse is IMintHouseEvents {
+    ///                                                          ///
+    ///                           ERRORS                         ///
+    ///                                                          ///
+
+    /// @dev Reverts if the function caller is not the manager.
+    error NOT_MANAGER();
+
+    /// @dev Reverts if address 0 is passed but not allowed
+    error ADDRESS_ZERO();
+
+    /// @dev Reverts if the supplied token ID for a bid does not match the mint's token ID.
+    error INVALID_TOKEN_ID();
+
+    /// @dev Reverts if the mint has already expired.
+    error DROP_EXPIRED();
+
+    /// @dev Reverts if bps is greater than 10,000.
+    error INVALID_BPS();
+
+    /// @dev Reverts if the creator rate is below the minimum required creator rate basis points.
+    error CREATOR_RATE_TOO_LOW();
+
+    /// @dev Reverts if the new minimum creator rate is not greater than the previous minimum creator rate.
+    error MIN_CREATOR_RATE_NOT_INCREASED();
+
+    /// @dev Reverts if the minimum creator rate is not less than or equal to the creator rate.
+    error MIN_CREATOR_RATE_ABOVE_CREATOR_RATE();
+
+    /// @dev Reverts if the mint start time is not set, indicating the mint hasn't begun.
+    error DROP_NOT_BEGUN();
+
+    /// @dev Reverts if the mint has already been settled.
+    error DROP_ALREADY_SETTLED();
+
+    /// @dev Reverts if the mint has not yet completed based on the current block timestamp.
+    error DROP_NOT_COMPLETED();
+
+    /// @dev Reverts if an existing mint is in progress.
+    error DROP_ALREADY_IN_PROGRESS();
+
+    struct Mint {
+        // ERC1155 token ID
+        uint256 tokenId;
+        // The current highest bid amount
+        uint256 amount;
+        // The time that the mint started
+        uint256 startTime;
+        // The time that the mint is scheduled to end
+        uint256 endTime;
+        // The address of the current highest bid
+        address payable bidder;
+        // The address of the referral account who referred the current highest bidder
+        address payable referral;
+        // Whether or not the mint has been settled
+        bool settled;
+    }
+
+    struct PaymentShares {
+        // Scaled means it hasn't been divided by 10,000 for BPS to allow for precision in division by
+        // consuming functions
+        uint256 creatorDirectScaled;
+        uint256 creatorGovernance;
+        uint256 owner;
+    }
+
+    struct PaidToCreators {
+        uint256 points;
+        uint256 eth;
+    }
+
+    /**
+     * @notice The mint parameters
+     * @param mintPrice The reserve price of each mint
+     * @param duration The duration of each mint
+     * @param creatorRateBps The creator rate basis points of each mint - the share of the winning bid that is reserved for the creator
+     * @param minCreatorRateBps The minimum creator rate basis points of each mint
+     */
+    struct MintParams {
+        uint256 timeBuffer;
+        uint256 mintPrice;
+        uint256 duration;
+        uint256 creatorRateBps;
+        uint256 minCreatorRateBps;
+    }
+
+    function createNewMint() external;
+
+    function pause() external;
+
+    function unpause() external;
+
+    function setMintPrice(uint256 mintPrice) external;
+
+    function setCreatorRateBps(uint256 _creatorRateBps) external;
+
+    function setMinCreatorRateBps(uint256 _minCreatorRateBps) external;
+
+    /**
+     * @notice Initialize the mint house and base contracts.
+     * @param initialOwner The address of the owner.
+     * @param mintParams The mint params for mints.
+     */
+    function initialize(address initialOwner, MintParams calldata mintParams) external;
+}
